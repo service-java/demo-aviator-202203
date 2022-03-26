b6658fa2d58adbac4530c33fdadacef6b8508c8d
luo0412 <1095847440@qq.com>
2022-03-26 13:00:48 +0000

    adjust

--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 # 参考 @ref
 
 - Java使用Aviator表达式 学习记录
+  - https://juejin.cn/user/2752832846435895/posts
   - https://juejin.cn/post/7002235897873694757
+
+- Aviator-入门教程 @coll
+  - https://juejin.cn/post/6949737576139849741
--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.googlecode.aviator.AviatorEvaluator;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,14 @@ public class IndexController {
     
     @RequestMapping("/")
     public Object index(Integer age) {
-        Long result = (Long) AviatorEvaluator.execute("1+2+3");
+        // Long result = (Long) AviatorEvaluator.execute("1+2+3");
+
+        String fun = "(a+b)/3";
+        Map map = new HashMap<>();
+        map.put("a",11);
+        map.put("b",22);
+        Object result = AviatorEvaluator.execute(fun, map);
+
         return result;
     }
 
